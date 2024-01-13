63eb0fb223772da8c2cb646dbf81146aa2df2bb6
nckvDev <jirapong.ku@rmuti.ac.th>
2024-01-13 18:04:36 +0700

    feat: tuple

---
+++ 02basic/myTupEnums.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const user: (string | number)[] = ['hc', 2]
+var tUser;
+tUser = ['nckv', 123, true];
+var rgb = [255, 255, 112];
+var newUser = [122, 'example@google.com'];
+newUser[1] = 'hc@g.com';
--- 02basic/myTupEnums.ts
+++ 02basic/myTupEnums.ts
@@ -1,1 +1,15 @@
-const user: (string | number)[] = ['hc', 2]
+// const user: (string | number)[] = ['hc', 2]
+let tUser: [string, number, boolean]
+
+tUser = ['nckv', 123, true]
+
+let rgb: [number, number, number] = [255, 255, 112]
+
+type User = [number, string]
+
+const newUser: User = [122, 'example@google.com']
+
+newUser[1] = 'hc@g.com'
+// newUser.push(true)
+
+export {}
